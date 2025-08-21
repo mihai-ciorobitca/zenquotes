d6ce1a85a427b226d7082f104d009dd9058edd0f
Mihai Ciorobitca <mihai.ciorobitca@outlook.com>
2025-08-21 10:57:37 +0200

    m

--- api/index.js
+++ api/index.js
@@ -1,20 +1,13 @@
 const express = require("express");
-const fetch = require("node-fetch");
+const { faker } = require("@faker-js/faker");
+
 const app = express();
 
-// Root route → fetch random quote
-app.get("/", async (req, res) => {
-  try {
-    const url = "https://zenquotes.io/api/random";
-    const response = await fetch(url);
-    const data = await response.json();
-
-    res.json({ message: data[0].q });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch quote" });
-  }
+app.get("/", (req, res) => {
+  const randomQuote = faker.lorem.sentence();
+  res.json({ message: randomQuote });
 });
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+  console.log("Server is running on port 3000");
 });
